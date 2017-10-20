68fa97b6fa2ec732197eb07b433844d743f03e9c
SkillAlex1984 <dd050484fav1@gmail.com>
2017-10-20 21:03:43 +0300

    menu bootstrap

---
+++ lesson5/alert.js
@@ -0,0 +1,12 @@
+"use strict"
+//alert( 'Привет, Мир!' );
+var admin;
+var name;
+var user = 'John',
+    age = 25,
+    message = 'Hello';
+
+name = 'Vasilj';
+admin = 123;
+alert(typeof admin);
+//alert(message2);
