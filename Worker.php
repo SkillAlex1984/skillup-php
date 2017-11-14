eee00f93e9fd6fa39b9f60e2b58e7a39d72a27a4
SkillAlex1984 <dd050484fav1@gmail.com>
2017-11-14 23:15:23 +0400

    lesson 12 - object

---
+++ Worker.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SkillUP student
+ * Date: 14.11.2017
+ * Time: 22:59
+ */
+
+class Worker
+{
+    public $name;
+    public $age;
+    public $salary;
+}
--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
-include 'lib.php';
-include 'User.php';
+include 'Worker.php';
+
+$obj1 = new Worker();
+$obj1->name = 'Иван';
+$obj1->age = 25;
+$obj1->salary = 1000;
+
+$obj2 = new Worker();
+$obj2->name = 'Вася';
+$obj2->age = 26;
+$obj2->salary = 2000;
+
+echo $obj1->salary + $obj2->salary;
+
+/*include 'User.php';
 include 'PremiumUser.php';
 $user = new PremiumUser('Tecn');
 
@@ -20,4 +33,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 }
 
-include 'form.php';
+include 'form.php';*/
