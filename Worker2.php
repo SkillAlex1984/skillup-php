12b8137ac56247841c86a3114c441dd1633dedb7
SkillAlex1984 <dd050484fav1@gmail.com>
2017-11-15 22:28:56 +0200

    homework 12: object

---
+++ Worker2.php
@@ -0,0 +1,37 @@
+<?php
+
+class Worker2
+{
+    private $name;
+    private $age;
+    private $salary;
+
+    public function getName() {
+        return $this->name;
+    }
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function setAge($age) {
+        if ($this->checkAge($age)) {
+            $this->age = $age;
+        }
+    }
+    public function getAge(){
+        return $this->age;
+    }
+    public function setSalary ($salary){
+        $this->salary = $salary;
+    }
+    public function getSalary () {
+        return $this->salary;
+    }
+    private function checkAge($age)
+    {
+        if ($age > 1 && $age < 100) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
 include 'Worker.php';
+include 'Worker2.php';
+include 'Worker3.php';
 
+// Задача 1
 $obj1 = new Worker();
 $obj1->name = 'Иван';
 $obj1->age = 25;
@@ -12,7 +15,33 @@ $obj2->name = 'Вася';
 $obj2->age = 26;
 $obj2->salary = 2000;
 
-echo $obj1->salary + $obj2->salary;
+echo 'Сумма зарплат Ивана и Васи из первой задачи ', $obj1->salary + $obj2->salary . '</br> ';
+
+// Задача 2
+$worker2 = new Worker2();
+$worker2->setName('Иван');
+$worker2->setAge(25);
+$worker2->setSalary(1000);
+
+$worker22 = new Worker2();
+$worker22->setName('Вася');
+$worker22->setAge(26);
+$worker22->setSalary(2000);
+
+echo 'Сумма зарплат Ивана и Васи из второй задачи ', $worker2->getSalary() + $worker22->getSalary() . '</br> ';
+echo 'Сумма возрастов Ивана и Васи из второй задачи ', $worker2->getAge() + $worker22->getAge() . '</br> ';
+
+// Задача 3
+
+$worker22->setAge(300);
+echo 'возраст не корректный - он не должен меняться ', $worker22->getAge() . '</br>';
+
+// Задача 4
+
+$worker4 = new Worker3('Dima', 1000);
+$worker4->age = 25;
+echo 'Произведение возраста и зарплаты ', $worker4->age * $worker4->getSalary();
+
 
 /*include 'User.php';
 include 'PremiumUser.php';
