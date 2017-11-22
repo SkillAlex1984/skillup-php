d6dc7bb18e8f66f7c2621a54653347ec1eab0b8f
SkillAlex1984 <dd050484fav1@gmail.com>
2017-11-22 22:59:54 +0200

    homework 14: cookie, session

--- form2.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: MY
- * Date: 22.11.2017
- * Time: 21:20
- */
-echo 'ffffffffffffffffff2';
--- init.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-if ( isset($_SESSION['user']) && $_SERVER['PHP_SELF'] != '/form2.php') {
-    header(('Location: form2.php'));
-}
