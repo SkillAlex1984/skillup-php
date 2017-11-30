3120857ebc937ea8c58ae1b535b6b0fcc732df36
SkillAlex1984 <dd050484fav1@gmail.com>
2017-11-30 19:08:56 +0200

    homework 16: homerwork16, SQL, JOIN

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 
 include 'Worker.php';
-
-
 $pdo = new PDO('mysql:host=localhost;dbname=skillup;charset=utf8','root', '');
 //$sql = 'SELECT * FROM workers WHERE id = 3';
 $salary = 'SELECT * FROM workers WHERE salary = 500';
-//$result = $pdo->query($salary); это простой вариант
-$result = $pdo->prepare($salary);  // это подготовленый
-$result->execute(['salary' => 500]); // запрос
+$result = $pdo->query($salary);
 
 while ($row = $result->fetchObject(Worker::class)) {
     var_dump($row);
